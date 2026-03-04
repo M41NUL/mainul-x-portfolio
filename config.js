4ee9906d10e821cbfef4eace73bf8c6a31dd7d43
MAINUL - X <githubmainul@gmail.com>
2026-03-04 18:04:43 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+/*
+MAINUL-X Portfolio - Configuration
+Author: Md. Mainul Islam (MAINUL-X)
+GitHub: https://github.com/M41NUL
+*/
+
+const CONFIG = {
+    BOT_TOKEN: '8213501580:AAH20G20yuUPie6j97GNRMZca30xHSZYzfs',
+    CHANNEL_ID: '@mainulx_updates',
+    CHAT_ID: '8279574441',
+    GITHUB: 'https://github.com/m41nul',
+    WEBSITE: 'https://m41nul.github.io'
+};
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = CONFIG;
+}
