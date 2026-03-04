b519acf8f6929f90f1a2be59bc7aa87073af4f22
MAINUL - X <githubmainul@gmail.com>
2026-03-04 15:57:52 +0600

    Create chatbot-config.js

---
+++ chatbot/chatbot-config.js
@@ -0,0 +1,32 @@
+// Chatbot configuration
+
+const chatbotConfig = {
+    // Basic settings
+    botName: "MAINUL-X Helper",
+    welcomeMessage: "👋 Welcome to MAINUL-X Helpbot!",
+    
+    // Language settings
+    defaultLanguage: "bn", // "bn" or "en"
+    
+    // Time settings
+    showTime: true,
+    
+    // Typing effect
+    showTypingIndicator: true,
+    typingSpeed: 50,
+    
+    // Quick buttons
+    showQuickButtons: true,
+    
+    // Contact info
+    whatsappNumber: "8801308850528",
+    telegramUsername: "mdmainulislaminfo",
+    emailAddress: "githubmainul@gmail.com",
+    
+    // Social links
+    socialLinks: {
+        github: "https://github.com/m41nul",
+        facebook: "https://facebook.com/mainulxhy",
+        instagram: "https://instagram.com/mainul_xhy"
+    }
+};
